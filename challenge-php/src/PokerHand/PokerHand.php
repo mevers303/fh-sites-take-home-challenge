47fe34f1b8fc4bb0c8307bef74d3fa2501905e2c
Mark Evers <mevers303@gmail.com>
2019-03-13 12:24:30 -0600

    Created a WrongNumberOfCardsException and added it to the sanity checks.

--- challenge-php/src/PokerHand/PokerHand.php
+++ challenge-php/src/PokerHand/PokerHand.php
@@ -2,10 +2,44 @@
 
 namespace PokerHand;
 
+
+class WrongNumberOfCardsException extends Exception {
+
+    protected $_nCards;
+
+    public function __construct($nCards)
+    {
+        parent::__construct();
+        $this->_nCards = $nCards;
+    }
+
+    public function errorMessage()
+    {
+      //error message
+      $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile()
+      .': Invalid number of cards: <b>' . $this->_nCards . '</b>';
+      return $errorMsg;
+    }
+
+  }
+
+
 class PokerHand
 {
     public function __construct($hand)
     {
+
+        $cards = explode(" ", $hand);
+
+        // let's make sure we're playing real poker here, do some sanity checks...
+
+        // check if we have the right number of cards
+        if (count($cards) != 5)
+        {
+            throw new WrongNumberOfCardsException(count($cards));
+        }
+        
+
     }
 
     public function getRank()
