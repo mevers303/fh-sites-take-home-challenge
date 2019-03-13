1c377cac0cb6b4663497e557d389d35929655795
Mark Evers <mevers303@gmail.com>
2019-03-13 12:57:35 -0600

    Fixed a bug in debug output

--- challenge-php/src/PokerHand/PokerHand.php
+++ challenge-php/src/PokerHand/PokerHand.php
@@ -3,7 +3,7 @@
 namespace PokerHand;
 
 
-class WrongNumberOfCardsException extends Exception {
+class WrongNumberOfCardsException extends \Exception {
 
     protected $_nCards;
 
@@ -15,13 +15,63 @@ class WrongNumberOfCardsException extends Exception {
 
     public function errorMessage()
     {
-      //error message
-      $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile()
-      .': Invalid number of cards: <b>' . $this->_nCards . '</b>';
-      return $errorMsg;
+        return "Error on line {$this->getLine()} in {$this->getFile()}: Invalid number of cards: <b>{$this->_nCards}</b>";
     }
 
-  }
+}
+
+
+class Card
+{
+
+    public function __construct($card_str)
+    {
+
+        $card_str = strtolower($card_str);
+        if (strlen($card_str) < 2 || strlen($card_str) > 3)
+        {
+            throw new \UnexpectedValueException("The length of the card string was unexpected: {$card_str}");
+        }
+
+        $suit = $card_str[-1];
+        switch ($suit)
+        {
+            case 'c':
+            case 's':
+            case 'h':
+            case 'd':
+                // no reason to be concerned
+                break;
+            default:
+                // they gave us something unexpected
+                throw new \UnexpectedValueException("Unknown suit: {$suit} <{$card_str}>");
+        }
+
+        $face = substr($card_str, 0, -1);
+        switch ($face)
+        {
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '10':
+            case 'j':
+            case 'q':
+            case 'k':
+            case 'a':
+                // no reason to be concerned
+                break;
+            default:
+                // they gave us something unexpected
+                throw new \UnexpectedValueException("Unknown face: {$face} <{$card_str}>");
+        }
+
+    }
+}
 
 
 class PokerHand
@@ -38,7 +88,7 @@ class PokerHand
         {
             throw new WrongNumberOfCardsException(count($cards));
         }
-        
+
 
     }
 
